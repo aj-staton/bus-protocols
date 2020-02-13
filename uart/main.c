304facf1fa610c617a7fa48e11700bb35f97e961
aj-staton <ajstaton@cocsce-l3d22-10.ds.sc.edu>
2020-02-13 11:18:55 -0500

    Added qyestions for the TA.

--- uart/main.c
+++ uart/main.c
@@ -1,12 +1,21 @@
+/* Copyright 2020 Charles Daniels, Jason Bakos, Philip Conrad */
+
+/* Demonstration of asynchronous UART receive. Maintains a ring buffer of
+ * data read in by the UART interrupt, and displays it's contents from a
+ * foreground loop each interval. */
+
+#include <avr/interrupt.h>
 #include <avr/io.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <util/delay.h>
-#include <avr/io.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
+#define BUFSZ 64
+#define INTERVAL_MS 1000
+
+char buf[BUFSZ+1];
+uint8_t cursor;
 
 /**
  * @brief output one character via UART, on UART0
@@ -17,24 +26,13 @@
  * @return
  */
 int uart_putchar(char c, FILE *stream) {
-    /* wait for the transmitter to become ready */
-    loop_until_bit_is_set(UCSR0A, UDRE0);
-
-    /* because it is assumed by most serial consoles, we automatically
-     * rewrite plain LF endings to CRLF line endings */
-    if (c == '\n') {
-        uart_putchar('\r', stream);
-    }
-
-    /* wait for the transmitter to become ready, since we might have just
-     * transmitted a CR */
-    loop_until_bit_is_set(UCSR0A, UDRE0);
-
-    /* transmit the character */
-    UDR0 = c;
-
-    /* signal success */
-    return 0;
+        while(!(UCSR0A&(1<<UDRE0))){}; //wait while previous byte is completed
+        if (c == '\n') {
+                uart_putchar('\r', stream);
+        }
+        loop_until_bit_is_set(UCSR0A, UDRE0);
+        UDR0 = c;
+        return 0;
 }
 
 /**
@@ -42,95 +40,111 @@ int uart_putchar(char c, FILE *stream) {
  *
  * @param stream for compatibility
  *
- * @return the character that was read
+ * @return
  */
 int uart_getchar(FILE *stream) {
-
-    /* wait for the data to arrive */
-    loop_until_bit_is_set(UCSR0A, RXC0);
-
-    /* return the received data */
-    return UDR0;
-
+        while(!(UCSR0A & (1<<RXC0))){} // wait until data comes
+        return UDR0;
 }
 
-/**
- * @brief Initialize UART for use with stdio
- */
-void uart_init(void) {
-    /* initialize USART0 */
-    UBRR0=(((F_CPU/(UART_BAUDRATE*16UL)))-1); // set baud rate
-    UCSR0B|=(1<<TXEN0); //enable TX
-    UCSR0B|=(1<<RXEN0); //enable RX
 
-    UCSR0C |= (1 << UCSZ01) | (1 << UCSZ00); // character size of 8
-    UCSR0C &= ~(1 << USBS0); // 1 stop bit
-    UCSR0C &= ~( (1 << UPM01) | (1 << UPM00) ); // disable parity
+/* interrupt called when a character is received from UART */
+ISR (USART_RX_vect) {
 
-    /* initialize file descriptors, notice the functions we wrote earlier
-     * are used as callbacks here */
-    fdevopen(uart_putchar, NULL);
-    fdevopen(NULL, uart_getchar);
-    printf("\n\nUART initialized (%i 8N1)\n", UART_BAUDRATE);
+	/* read the new character in under the cursor */
+	buf[cursor] = UDR0;
+
+	/* guarantee null termination */
+	buf[BUFSZ+1] = '\0';
+	
+	/* advance the cursor */
+	cursor = (cursor + 1) % BUFSZ;
 }
 
-/**
- * @breif Sets the appropriate input and output pins on the Atmega board.
- */
-void inout_init(void) {
-  // Sets PCO as an output.
-  DDRC |= (1 << 0);
+void init(void) {
+        // initialize USART
+        UBRR0=(((F_CPU/(UART_BAUDRATE*16UL)))-1); // set baud rate
+        UCSR0B|=(1<<TXEN0); //enable TX
+        UCSR0B|=(1<<RXEN0); //enable RX
 
+        UCSR0C |= (1 << UCSZ01 ) | (1 << UCSZ00 ) ; // character size of 8
+        UCSR0C &= ~(1 << USBS0 ) ; // 1 stop bit
+        UCSR0C &= ~( (1 << UPM01 ) | (1 << UPM00 ) ); // disable parity
+        UCSR0B|=(1<<RXCIE0); // enable RX interrupt
+
+        // initialize file descriptors
+        fdevopen(uart_putchar, NULL);
+        fdevopen(NULL, uart_getchar);
+
+	// clear the buffer
+	for (int i = 0 ; i < BUFSZ ; i++) { buf[i] = ' '; }
+
+	cursor = 0;
+
+	// enable interrupts globally
+	sei();
 }
 
 int main(void) {
-  inout_init();
-  uart_init();
+	init();
+        int state = 0;
+	buf[0] = '\0';
+        cursor = 0;
 
-  char buf[65];
-  printf("im ready>\n");
-  
-  // Two states are possible:
-  //   0 = LED isn't blinking.
-  //   1 = LED is blinking.  
-  int state = 0;
- 
-  while(1) { 
-    scanf("%s", &buf);
-    // buf = uart_getchar();
-    // Check and set the current state.
-    if (strcmp(buf,"on") ==  0) {
-      printf("starting blinking\n");
-      // Update state.
-      state = 1;
-      // Clear buffer char array. 
-      memset(&buf[0], 0, sizeof(buf));   
-    } else if (strcmp(buf,"off") == 0) {
-      printf("stopped blinking\n");
-      // Update state.
-      state = 0;
-      // Clear buffer.
-      memset(&buf[0], 0, sizeof(buf));
-    } else if (strlen(buf) == 0) {
-      state = state; 
-    } else {
-      printf("Hello user! That command was wrong.\n");
-      memset(&buf[0], 0, sizeof(buf)); 
-    }
-    // Continute the current state if it is correct.
-    if (state == 0) { // No blink.
-      while (UCSR0A & (1 << RXC0) == 0) {
-        PORTC &= 0;
-      }
-    } else if (state == 1) { // Blink.
-      while (UCSR0A & (1 << RXC0) == 0) { // If this logic is 1, then a char in in buffer.
-        PORTC |= 1;
-        _delay_ms(500);
-        PORTC &= ~(1 << 0);
-        _delay_ms(500);
-      }
-    } else {
-      printf("Something is very wrong.");
-    }
-  }
+	while (1) {
+                /*
+		Do we use get/put_char()? If yes, what's the param?
+		Does UDRO give you a characer in the buffer?
+ 		Do our strcmps need a \n?
+  		Why is the terminal output strange?
+		*/
+		_delay_ms(INTERVAL_MS);
+		printf("cursor=%3u buf='%s'\n", cursor, buf);
+
+		// Read in register to buffer  
+		if (UCSR0A & (1 << RXC0) == 0){ 
+			// until \n is found, fill buffer
+			buf[cursor] = UDR0;
+			cursor++;
+			
+
+			//  Evaluate Buffer - Do we want our comparison nested or not?? (more cycles)  
+	    		if (strcmp(buf,"on\n") ==  0) {
+	      			printf("starting blinking\n");
+	      			// Update state.
+	      			state = 1;
+	      			// Clear buffer char array. 
+	      			memset(&buf[0], 0, sizeof(buf)); 
+			}
+			else if (strcmp(buf,"off\n") == 0) {
+			      printf("stopped blinking\n");
+			      // Update state.
+			      state = 0;
+			      // Clear buffer.
+			      memset(&buf[0], 0, sizeof(buf));
+			}
+			else {
+				printf("Invalid command, not changing state\n");
+				memset(&buf[0], 0, sizeof(buf));	
+			}
+			buf[0] = '\0';
+        		cursor = 0;
+		}
+                
+		// EXECUTE FUNCTION BASED ON SET STATE 
+		if (state == 0) { //dont blink
+			// set the thing to 0  
+			PORTC &= 0;
+			
+		}
+		else if (state == 1) { //blink
+			PORTC |= 1;
+        		_delay_ms(500);
+        		PORTC &= ~(1 << 0);
+        		_delay_ms(500);
+		}
+		else{
+		        printf("your state isnt on or off\n");
+		}
+	}
 }
