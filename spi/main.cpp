986f39e2331264fd0d87a931268d3099910c64e2
aj-staton <ajstaton0@gmail.com>
2020-04-03 15:11:04 -0400

    simplified version

--- spi/main.cpp
+++ spi/main.cpp
@@ -6,14 +6,11 @@
 
 /* number of SPI SCK cycles per system clock cycle */
 #define SPI_CLK_RATIO 16
-#define MASK(byte, mask) (byte & mask)
-
 
 typedef enum {
   TRUE,
   FALSE
 } bool_t;
-
 
 bool_t is_high(uint8_t byte, uint8_t N);
 uint8_t spi_read(simulation_state* s, uint8_t addr);
@@ -35,56 +32,51 @@ int main(int argc, char **argv) {
 
 /*
  *  @brief -- a bit-banged representation of SPI.
- *  This will be written where CPOL = 1, CPHA = 1, (Mode 3)
+ *  This will be written where CPOL = 1, CPHA = 1 (Mode 3)
  *  and the MSB is transmitted first.
  *  
  *  @params: s   -- the hardware simulation on Verilator.
- *           req -- the address to read from the sensor.
+ *           addr -- the adress to read from the sensor.
  */
-
 uint8_t spi_read(simulation_state* s, uint8_t addr) {
- 
- uint8_t data = 0x00;
- // IO_SCK is supposed to be active-low. I don't want
- // and immediate sample at the first pulldown of CS.
- write_io(s, IO_SCK, 1);
- delay_cycles(s, 1);
- // Pull CS Low to signal transaction.
- write_io(s, IO_CS, 0);
-
- /* Transactions:  *  First: Write the address to the sensor on MOSI.
-  *  Second: Read the data from the sensor on MISO. */
- // The Write
- for (int i = 15; i >= 0; --i) {
-   write_io(s, IO_CS, 0);
-   if (i % 2 == 1) { // This is NOT active
-     write_io(s, IO_SCK, 1);
-   } else {
-     write_io(s, IO_SCK, 0);
-     if (is_high(addr, i/2) == TRUE) {
-       write_io(s, IO_MOSI, 1);
-     } else {
-       write_io(s, IO_MOSI, 0);
-     }
-   }
-   delay_cycles(s, 1);
- }
- // The Read
- for (int i = 15; i >= 0; --i) {
-   write_io(s, IO_CS, 0);
-   if (i % 2 == 1) { // This is NOT active
-     write_io(s, IO_SCK, 1);
-   } else {
-     write_io(s, IO_SCK, 0);
-     data |= (read_io(s, IO_MISO) << (i/2+1));
-   }
-   delay_cycles(s, 1);
- }
- // Pull CS high--transaction ended.
- write_io(s, IO_CS, 1);
- return data;
-
+  uint8_t data = 0x00;
+  // IO_SCK is supposed to be active-low. I don't want
+  // and immediate sample at the first pulldown of CS.
+  write_io(s, IO_SCK, 1);
+  delay_cycles(s, 1);
+  // Pull CS Low to signal transaction.
+  write_io(s, IO_CS, 0);
+  delay_cycles(s, SPI_CLK_RATIO);
+  /* Transactions:  
+   *  First Loop: Write the address to the sensor on MOSI.
+   *  Second Loop: Read the data from the sensor on MISO. */
+  for (int i = 7; i >= 0; --i) {
+    // Make sure clock is at zero for 8 cycles.
+    write_io(s, IO_SCK, 0);
+    // Write to MOSI:
+    write_io(s, IO_MOSI, ((addr >> i) & 0x01));
+    delay_cycles(s, SPI_CLK_RATIO/2);
+    // Make sure clock is at one for 8 cycles.
+    write_io(s, IO_SCK, 1);
+    delay_cycles(s, SPI_CLK_RATIO/2);
+  }
+  for (int i = 7; i >= 0; --i) {
+    // Make sure clock is at zero for 8 cycles.
+    write_io(s, IO_SCK, 0);
+    // Write to MOSI:
+    write_io(s, IO_MOSI, 1); // Dummy bits.
+    delay_cycles(s, SPI_CLK_RATIO/2);
+    // Make sure clock is at one for 8 cycles.
+    // Read MISO:
+    data |= (read_io(s, IO_MISO) << i);
+    write_io(s, IO_SCK, 1);
+    delay_cycles(s, SPI_CLK_RATIO/2);
+  }
+  // Pull CS high--transaction ended.
+  write_io(s, IO_CS, 1);
+  return data;
 }
+
 /*
  * @breif is_high() will get determine if the Nth bit of
  *   byte is 1 or 0. N is wrt big-endian order.
