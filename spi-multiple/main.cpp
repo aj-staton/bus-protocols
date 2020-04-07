e58975405a27188b3f783174960f0a071ad631cf
aj-staton <ajstaton0@gmail.com>
2020-04-07 15:25:53 -0400

    framework

---
+++ spi-multiple/main.cpp
@@ -0,0 +1,18 @@
+/* Copyright 2020 Jason Bakos, Philip Conrad, Charles Daniels */
+
+/* Used and modified by Charles Daniels for CSCE317, Project lab_virtspi_multi
+ *
+ * HINT: your name is not "Charles Daniels"
+ */
+
+#include "support.h"
+
+/* number of SPI SCK cycles per system clock cycle */
+#define SPI_CLK_RATIO 16
+
+int main(int argc, char **argv) {
+	simulation_state* s = initialize_simulation(argc, argv);
+
+	/* Your code here */
+
+}
